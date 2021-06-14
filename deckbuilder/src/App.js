c7e187a01c3d762ad48a0e9e8fcd429ed6ace7c7
Andrew Pham <anp6338@g.harvard.edu>
2021-06-14 15:49:17 -0600

    paper added

--- deckbuilder/src/App.js
+++ deckbuilder/src/App.js
@@ -1,41 +1,49 @@
-import React from 'react';
+import { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import IconButton from '@material-ui/core/IconButton';
+import './App.css'
 
-class App extends React.Component{
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(20),
+      height: theme.spacing(30),
+    },
+  },
+}));
 
-  constructor(){
-    super()
-    this.state={
-      d:[]
-    }
-    this.componentDidMount=this.componentDidMount.bind(this)
+export default function App() {
+  const [cards, setCards] = useState([])
+
+  async function fetchCards(){
+    let res = await fetch('https://api.magicthegathering.io/v1/cards?setName=kaldheim')
+    let json = await res.json()
+    setCards(json)
+    console.log(json)
   }
 
+  useEffect(() => {
+    fetchCards()
+  }, [])
 
-  async componentDidMount(){
-    await fetch('https://api.magicthegathering.io/v1/cards')
-    .then(res=>res.json())
-    .then((data)=>{
-      this.setState({d: data})
-    })
-    //.then(a => console.log(a));
-    console.log(this.state.d.cards[0].name)
-  }
+  const classes = useStyles();
   
-  render(){
-    if(this.state.d.cards)
-      return(
-        <div>
-          {
-            this.state.d.cards.map((card) => (
-            <div>
-              {card.name}
-            </div>
-          ))}
-        </div>
-      )
-    else
-      return(<div>Loading...</div>)
-  }
+  if(cards.cards)
+    return (
+      <div className={classes.root}>
+        {cards.cards.map((card) => (
+          <Paper elevation={3} className="cards">
+            <img src={card.imageUrl} alt={card.name}/>
+            <IconButton><AddCircleOutlineIcon /></IconButton>
+          </Paper>
+        ))}
+      </div>
+    );
+  else
+    return(<div>Loading...</div>)
 }
-
-export default App;
